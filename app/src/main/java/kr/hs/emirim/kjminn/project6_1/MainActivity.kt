009e0998e58a4389cd1737db2a122f9a14a860c2
kjminn <kjminn2001@gmail.com>
2023-08-01 19:47:02 +0900

    직접풀어보기 6-1완성

--- app/src/main/java/kr/hs/emirim/kjminn/project6_1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kjminn/project6_1/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.DatePicker
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -15,12 +16,10 @@ import android.widget.TimePicker
 
 class MainActivity : AppCompatActivity() {
     lateinit var chrono1 : Chronometer
-    lateinit var btnStart : Button
-    lateinit var btnDone : Button
     lateinit var rg : RadioGroup
     lateinit var radioCal:RadioButton
     lateinit var radioTime:RadioButton
-    lateinit var calendar : CalendarView
+    lateinit var calendar : DatePicker
     lateinit var timePick : TimePicker
     lateinit var textResult : TextView
     var selectedYear : Int = 0
@@ -32,8 +31,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         chrono1 = findViewById(R.id.chrono1)
-        btnStart = findViewById(R.id.btnStart)
-        btnDone = findViewById(R.id.btnDone)
         rg = findViewById(R.id.rg)
         radioCal = findViewById(R.id.radioDate);
         radioTime = findViewById(R.id.radioTime);
@@ -41,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         timePick = findViewById(R.id.timePick)
         textResult = findViewById(R.id.textResult)
 
+        rg.visibility = View.INVISIBLE
         timePick.visibility = View.INVISIBLE
         calendar.visibility = View.INVISIBLE
 
@@ -54,21 +52,26 @@ class MainActivity : AppCompatActivity() {
             calendar.visibility = View.INVISIBLE
         }
 
-        btnStart.setOnClickListener {
+        chrono1.setOnClickListener {
+            rg.visibility = View.VISIBLE
             chrono1.base = SystemClock.elapsedRealtime()
             chrono1.start()
             chrono1.setTextColor(Color.RED)
         }
 
-        btnDone.setOnClickListener {
+        textResult.setOnLongClickListener {
             chrono1.stop()
             chrono1.setTextColor(Color.BLUE)
             textResult.setText("" +selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일 ")
             textResult.append("" + timePick.currentHour + "시 " + timePick.currentMinute + "분 ")
             textResult.append("예약됨")
+            rg.visibility = View.INVISIBLE
+            timePick.visibility = View.INVISIBLE
+            calendar.visibility = View.INVISIBLE
+            true
         }
 
-        calendar.setOnDateChangeListener { calendarView, year, month, day ->
+        calendar.setOnDateChangedListener { datePicker, year, month, day ->
             selectedYear = year
             selectedMonth = month + 1
             selectedDay = day
